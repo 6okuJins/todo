0649e5ee5389edee8924e9557f270b868b1e4374
6okuJins <d.ma@mail.utoronto.ca>
2022-06-02 19:56:31 -0400

    Make script that generates DOM element for a Todo

---
+++ src/pages/TodoDisplay.js
@@ -0,0 +1,40 @@
+/**
+ * Returns a DOM element displaying the todo's info.
+ * {@link todoDisplay}
+ * @param {Object} todo
+ * @return {Object} DOMElement
+ */
+const TodoDisplay = (todo) => {
+  const todoDisplay = document.createElement('div');
+  todoDisplay.classList.add('todo');
+  todoDisplay.dataset.uuid = todo.getID();
+
+  const checkBox = document.createElement('input');
+  checkBox.setAttribute('type', 'checkbox');
+  checkBox.checked = todo.getStatus();
+  if (checkBox.checked) {
+    todo.classList.add('completed');
+  }
+  checkBox.addEventListener('click', () => {
+    todo.toggleStatus();
+    todoDisplay.classList.toggle('completed');
+    console.log(todoDisplay.classList); // for debugging delete later
+  });
+
+  const title = document.createElement('div');
+  title.classList.add('title');
+  title.textContent = todo.getTitle();
+
+  const dueDate = document.createElement('input');
+  dueDate.setAttribute('type', 'date');
+  dueDate.classList.add('dueDate');
+  dueDate.addEventListener('click', () => {
+    todo.setDueDate(dueDate.value);
+    console.log(todo.getDueDate());
+    console.log(todo.getTitle());
+  }); // for debugging delete later
+
+  todoDisplay.append(checkBox, title, dueDate);
+  return todoDisplay;
+};
+export default TodoDisplay;
