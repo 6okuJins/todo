b5cf85af3ac37f0fafa98600bfd52d791e31f2c2
6okuJins <d.ma@mail.utoronto.ca>
2022-06-02 16:34:33 -0400

    Rename Project to TodoContainer
    - Singular object to store all Todo's rather than many Projects.
    - Add toJSON method
    - Add filterByWeek, filterByDay, and getProject methods

---
+++ src/classes/TodoContainer.js
@@ -0,0 +1,33 @@
+import { isSameWeek, isSameDay, parseISO } from 'date-fns';
+
+const TodoContainer = (todoContainer = [], todoContainerProjects = []) => {
+  let container = todoContainer;
+  const projects = todoContainerProjects;
+  const date = new Date();
+  const getContainer = () => [...container]; // shallow copy so can't modify container from
+  const addTodo = (newTodo) => container.push(newTodo);
+  const getTodoByUUID = (UUID) => container.filter((todo) => todo.getID() === UUID).pop();
+  const removeTodoByUUID = (UUID) => {
+    container = container.filter((todo) => todo.getID() !== UUID);
+  };
+  const getAllProjects = () => [...projects];
+  const getProject = (project) => container.filter((todo) => (todo.getProject() === project));
+  const filterByWeek = () => container.filter((todo) => isSameWeek(todo.getDueDate(), Date.now()));
+  const filterByDay = () => container.filter((todo) => isSameDay(parseISO(todo.getDueDate()), date));
+  const toJSON = () => ({
+    container: [...container],
+    projects: [...projects],
+  });
+  return {
+    toJSON,
+    getContainer,
+    addTodo,
+    getTodoByUUID,
+    removeTodoByUUID,
+    getAllProjects,
+    getProject,
+    filterByDay,
+    filterByWeek,
+  };
+};
+export default TodoContainer;
