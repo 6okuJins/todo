5519b9bb9d96b04b2e74c9a65deb4c80f3d5014f
6okuJins <d.ma@mail.utoronto.ca>
2022-06-06 19:33:47 -0400

    Make new Todo insert at the bottom of content display

--- src/functions/fillContent.js
+++ src/functions/fillContent.js
@@ -3,7 +3,7 @@ import { TodoDisplay } from '../pages';
 const fillContent = (container, contentNode) => {
   // eslint-disable-next-line no-plusplus
   for (let i = 0; i < container.length; i++) {
-    contentNode.insertBefore(TodoDisplay(container[i]), contentNode.firstChild);
+    contentNode.insertBefore(TodoDisplay(container[i]), contentNode.lastChild);
   }
 };
 export default fillContent;
