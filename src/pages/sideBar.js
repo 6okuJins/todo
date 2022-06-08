cfdd267f186239649ae99ae6223743d9b668d077
6okuJins <d.ma@mail.utoronto.ca>
2022-06-08 15:36:05 -0400

    Add methods to empty and fill projects container

--- src/pages/sideBar.js
+++ src/pages/sideBar.js
@@ -1,4 +1,5 @@
 import AddNewDisplay from './AddNewDisplay';
+
 const sideBarButton = (content) => {
   const button = document.createElement('button');
   button.classList.add('side-bar-button', content);
@@ -12,14 +13,36 @@ const sideBar = () => {
   const inboxButton = sideBarButton('Inbox');
   const todayButton = sideBarButton('Today');
   const weekButton = sideBarButton('Week');
+  sideBar.append(inboxButton, todayButton, weekButton);
 
   const projectDivider = document.createElement('div');
   projectDivider.textContent = 'Projects';
+
   const projectContainer = document.createElement('div');
   projectContainer.classList.add('project-container');
+
   const addProjectDisplay = AddNewDisplay('project');
 
-  sideBar.append(inboxButton, todayButton, weekButton, projectDivider, projectContainer, addProjectDisplay);
+  sideBar.emptySideBar = () => {
+    while (projectContainer.firstChild) {
+      projectContainer.removeChild(projectContainer.firstChild);
+    }
+  };
+  sideBar.fillSideBar = (projects) => {
+    for (let i = 0; i < projects.length; i++) {
+      const projectDiv = document.createElement('div');
+      projectDiv.classList.add('project');
+      const projectBtn = document.createElement('button');
+      projectBtn.classList.add('open-project');
+      projectBtn.textContent = projects[i];
+      const deleteButton = document.createElement('button');
+      deleteButton.textContent = 'x';
+      deleteButton.classList.add('delete');
+      projectDiv.append(projectBtn, deleteButton);
+      projectContainer.append(projectDiv);
+    }
+  };
+  sideBar.append(projectDivider, projectContainer, addProjectDisplay);
   return sideBar;
 };
 export default sideBar;
