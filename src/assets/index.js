5f63e1fe4bdef09964e66b40166246324dce54cf
6okuJins <d.ma@mail.utoronto.ca>
2022-08-04 20:48:47 -0400

    Add logo svg and give it gradient animation

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,2 +1,3 @@
 export { default as plusThick } from './plus-thick.svg';
 export { default as trashCan } from './trash-can-outline.svg';
+export { default as checkBox } from './checkbox-outline.svg';
