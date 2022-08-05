b40002bd65b018b6773c1cbea7e26fe21aab8197
6okuJins <d.ma@mail.utoronto.ca>
2022-08-04 20:51:20 -0400

    Add logo and project title to header

--- src/pages/header.js
+++ src/pages/header.js
@@ -1,6 +1,26 @@
+import { checkBox } from '../assets';
+
 const Header = () => {
   const header = document.createElement('div');
   header.classList.add('header');
+
+  const logo = document.createElement('div');
+  logo.textContent = 'Todo';
+  logo.classList.add('logo');
+  header.append(logo);
+  const logoImg = document.createElement('div');
+  logoImg.classList.add('img-container');
+  logoImg.innerHTML = checkBox;
+  logo.append(logoImg);
+
+  const projectTitle = document.createElement('div');
+  header.append(projectTitle);
+  projectTitle.classList.add('project-title');
+  projectTitle.textContent = 'Inbox';
+
+  const ruler = document.createElement('div');
+  header.append(ruler);
+  ruler.classList.add('rounded');
   return header;
 };
 export default Header;
