b95314090285a46dd4fab858630a540e34ab23f5
6okuJins <d.ma@mail.utoronto.ca>
2022-08-05 03:10:15 -0400

    Add more icons

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,3 +1,7 @@
 export { default as plusThick } from './plus-thick.svg';
 export { default as trashCan } from './trash-can-outline.svg';
 export { default as checkBox } from './checkbox-outline.svg';
+export { default as bulletIcon } from './format-list-bulleted-square.svg';
+export { default as inboxIcon } from './inbox.svg';
+export { default as todayIcon } from './calendar-today.svg';
+export { default as weekIcon } from './calendar-week.svg';
